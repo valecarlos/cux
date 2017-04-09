9c942c9248a54184b587c548a7c21cede4479e40
Carlos <carlos_valencia@live.com>
2017-04-09 16:20:42 -0500

    partial push changing layout

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
     $(".start-chat").on("click", toggleChatBox);
+    $(".navbar-toggle").on("click", toggleMenu);
 });
 
 function toggleChatBox(){
     $(".chat-box").toggleClass("visible");
     $(".start-chat i").toggleClass("fa-comment");
     $(".start-chat i").toggleClass("fa-times");
+}
+function toggleMenu(){
+    $("nav").toggleClass("visible");
 }
